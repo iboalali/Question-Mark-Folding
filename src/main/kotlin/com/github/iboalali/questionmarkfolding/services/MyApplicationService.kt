063df44cdae33986c7533891ec22bfa3e0b3f4d9
GitHub Action <action@github.com>
2022-04-22 07:52:07 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/iboalali/questionmarkfolding/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.iboalali.questionmarkfolding.services
+
+import com.github.iboalali.questionmarkfolding.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
