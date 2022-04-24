faa64e5e8237878d7bf72639b6f2052fe05aaf2b
Ibrahim Al-Alali <alali@live.de>
2022-04-24 12:05:49 +0200

    clean up

--- src/main/kotlin/com/github/iboalali/questionmarkfolding/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/iboalali/questionmarkfolding/listeners/MyProjectManagerListener.kt
@@ -1,13 +1,15 @@
 package com.github.iboalali.questionmarkfolding.listeners
 
-import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import com.github.iboalali.questionmarkfolding.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
+    init {
+        println("${this::class.simpleName} loaded")
+    }
+
     override fun projectOpened(project: Project) {
-        project.service<MyProjectService>()
+        println("Welcome to the folding of Question Marks project!")
     }
 }
